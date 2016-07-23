953682557f5a8796186c23d2e238a0e755e8a27a
seounghoon kim <seounghoon kim>
2016-07-23 14:13:01 +0900

    no message

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php 
-$db_name = "employee101";
-$mysql_username = "root";
+$db_name = "u459792371_4312";
+$mysql_username = "u459792371_4312";
 $mysql_password = "ksh930617";
-$server_name = "localhost";
+$server_name = "mysql.hostinger.kr";
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password,$db_name);
 
 ?>
