b807c3be7c29e7915b1f602c7f3702d68cdce8d3
nildy <faithdy@kw.ac.kr>
2016-08-01 19:25:39 +0900

    08-01 통합 ::directory:: 1.employee // 사업자에 관한 php 폴더 2.admin // 관리자에 관한 php 폴터 3.include // db연동 등 시스템 php 폴터

--- include/db_connect.php
+++ include/db_connect.php
@@ -8,12 +8,14 @@ function db_con()
 	$host = "localhost";
 	$user = "root";
 	$pwd = "i'mthere";
-
+  $db_name = "EM";
+  
 	$con = mysql_connect($host,$user,$pwd);
 	if($con==0) echo mysql_error();
 
-	mysql_select_db('EM', $con);
+	mysql_select_db($db_name, $con);
 	return $con;
 }
 
+$conn = db_con();
 ?>
